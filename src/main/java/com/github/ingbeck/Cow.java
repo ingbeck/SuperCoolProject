39f8812cc807cfd31c5628034b0fb58560b71749
ingobecker <mail@ingobecker.de>
2024-02-08 11:04:16 +0100

    add class Cow.java and add method melk()

---
+++ src/main/java/com/github/ingbeck/Cow.java
@@ -0,0 +1,53 @@
+package com.github.ingbeck;
+
+import java.util.Objects;
+
+public class Cow extends Animal{
+
+    private boolean isMilkable;
+
+    public boolean isMilkable() {
+        return isMilkable;
+    }
+
+    public void setMilkable(boolean milkable) {
+        isMilkable = milkable;
+    }
+
+    public Cow(String name, int age, String breed, boolean isMilkable) {
+        super(name, age, breed);
+        this.isMilkable = isMilkable;
+    }
+
+    public void milk() {
+        if (isMilkable) {
+            System.out.println("Milked " + getName());
+        } else {
+            System.out.println(getName() + " is not milkable");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cow{" +
+                "isMilkable=" + isMilkable +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", breed='" + breed + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Cow cow = (Cow) o;
+        return isMilkable == cow.isMilkable;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), isMilkable);
+    }
+}
