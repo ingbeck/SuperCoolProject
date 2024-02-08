e91e3eaf610bdf2491bd6c89eea97e31015dbff1
ingobecker <mail@ingobecker.de>
2024-02-08 10:42:44 +0100

    add class Animal.java

---
+++ src/main/java/com/github/ingbeck/Animal.java
@@ -0,0 +1,60 @@
+package com.github.ingbeck;
+import java.util.Objects;
+
+public class Animal {
+    String name;
+    int age;
+    String breed;
+
+    public Animal(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && Objects.equals(name, animal.name) && Objects.equals(breed, animal.breed);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, breed);
+    }
+}
