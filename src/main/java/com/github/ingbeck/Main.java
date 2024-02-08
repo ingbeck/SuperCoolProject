d16aa7ff8f0cb22c48b7e88ab4ab1215080a1677
ingobecker <mail@ingobecker.de>
2024-02-08 10:51:54 +0100

    add 2 instances of Object 'Animal' and compare, if it is the same object

--- src/main/java/com/github/ingbeck/Main.java
+++ src/main/java/com/github/ingbeck/Main.java
@@ -2,6 +2,8 @@ package com.github.ingbeck;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal1 = new Animal("Rex", 5, "Bulldog");
+        Animal animal2 = new Animal("Pumba", 4, "Wild Boar");
+        System.out.println(animal1.equals(animal2));
     }
 }
